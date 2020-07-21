5b3b13e38f80160837d2cffaf3fa6a27c3468001
Philippe Dubois <lp.dubois@xefi.fr>
2020-07-21 15:27:32 +0200

    Fixed template indentation

--- src/templates/app_module.template.ts
+++ src/templates/app_module.template.ts
@@ -9,15 +9,15 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    navigatorKey: Modular.navigatorKey,
-    title: 'Flutter Project',
-    theme: ThemeData(
-        primarySwatch: Colors.blue,
-    ),
-    initialRoute: '/',
-    onGenerateRoute: Modular.generateRoute,
+      navigatorKey: Modular.navigatorKey,
+      title: 'Flutter Project',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: Modular.generateRoute,
     );
- }
+  }
 } 
 
 main() => runApp(ModularApp(module: AppModule()));
@@ -33,20 +33,20 @@ import 'app_widget.dart';
 import 'modules/home/home_module.dart';
 
 class AppModule extends MainModule {
-@override
-List<Bind> get binds => [
+  @override
+  List<Bind> get binds => [
         Bind((i) => AppPresenter()),
-    ];
+      ];
 
-@override
-List<Router> get routers => [
+  @override
+  List<Router> get routers => [
         Router(HomeModule.route, module: HomeModule()),
-    ];
+      ];
 
-@override
-Widget get bootstrap => AppWidget();
+  @override
+  Widget get bootstrap => AppWidget();
 
-static Inject get to => Inject<AppModule>.of();
+  static Inject get to => Inject<AppModule>.of();
 }
 `;
   }
--- src/templates/full_module.template.ts
+++ src/templates/full_module.template.ts
@@ -16,15 +16,15 @@ import 'package:${this.projectName.toLowerCase()}/core/page_state.dart';
 import '${this.moduleNameSnake}_presenter.dart';
 
 class ${this.moduleNamePascal}Page extends StatefulWidget {
-    static String route = "/";
-    final String title;
-    const ${this.moduleNamePascal}Page({
+  static String route = "/";
+  final String title;
+  const ${this.moduleNamePascal}Page({
     Key key,
     this.title = "${this.moduleNamePascal}",
-    }) : super(key: key);
+  }) : super(key: key);
 
-    @override
-    _${this.moduleNamePascal}PageState createState() => _${
+  @override
+  _${this.moduleNamePascal}PageState createState() => _${
       this.moduleNamePascal
     }PageState();
 }
@@ -32,17 +32,17 @@ class ${this.moduleNamePascal}Page extends StatefulWidget {
 class _${this.moduleNamePascal}PageState extends PageState<${
       this.moduleNamePascal
     }Page, ${this.moduleNamePascal}Presenter> {
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text(widget.title),
-        ),
-        body: Column(
+      ),
+      body: Column(
         children: <Widget>[],
-        ),
+      ),
     );
-    }
+  }
 }
 `;
   }
@@ -55,17 +55,17 @@ import '${this.moduleNameSnake}_presenter.dart';
 
 class ${this.moduleNamePascal}Module extends ChildModule {
     static String route = "${this.moduleRoute}";
-    @override
-    List<Bind> get binds => [
+  @override
+  List<Bind> get binds => [
         Bind((i) => ${this.moduleNamePascal}Presenter()),
-        ];
+      ];
 
-    @override
-    List<Router> get routers => [
+  @override
+  List<Router> get routers => [
         Router(${this.moduleNamePascal}Page.route, child: (_, args) => ${this.moduleNamePascal}Page()),
-        ];
+      ];
 
-    static Inject get to => Inject<${this.moduleNamePascal}Module>.of();
+  static Inject get to => Inject<${this.moduleNamePascal}Module>.of();
 }
 `;
   }
